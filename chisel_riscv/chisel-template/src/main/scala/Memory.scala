3e227948932b2101ea50cf5904c91d61da422dde
msBRF65 <msaito6525@gmail.com>
2022-11-26 22:15:21 +0900

    add: lw, sw

--- chisel_riscv/chisel-template/src/main/scala/Memory.scala
+++ chisel_riscv/chisel-template/src/main/scala/Memory.scala
@@ -10,14 +10,22 @@ class ImemPortIo extends Bundle {
   val inst = Output(UInt(WORD_LEN.W))
 }
 
+class DmemPortIo extends Bundle {
+  val addr = Input(UInt(WORD_LEN.W))
+  val rdata = Output(UInt(WORD_LEN.W))
+  val wen = Input(Bool())
+  val wdata = Input(UInt(WORD_LEN.W))
+}
+
 class Memory extends Module {
   val io = IO(new Bundle {
     val imem = new ImemPortIo
+    val dmem = new DmemPortIo
   })
 
   val mem = Mem(16384, UInt(8.W))
 
-  loadMemoryFromFile(mem, "src/hex/fetch.hex")
+  loadMemoryFromFile(mem, "src/hex/sw.hex")
 
   io.imem.inst := Cat(
     mem(io.imem.addr + 3.U(WORD_LEN.W)),
@@ -25,4 +33,18 @@ class Memory extends Module {
     mem(io.imem.addr + 1.U(WORD_LEN.W)),
     mem(io.imem.addr)
   )
+
+  io.dmem.rdata := Cat(
+    mem(io.dmem.addr + 3.U(WORD_LEN.W)),
+    mem(io.dmem.addr + 2.U(WORD_LEN.W)),
+    mem(io.dmem.addr + 1.U(WORD_LEN.W)),
+    mem(io.dmem.addr)
+  )
+
+  when(io.dmem.wen) {
+    mem(io.dmem.addr) := io.dmem.wdata(7, 0)
+    mem(io.dmem.addr + 1.U) := io.dmem.wdata(15, 8)
+    mem(io.dmem.addr + 2.U) := io.dmem.wdata(23, 16)
+    mem(io.dmem.addr + 3.U) := io.dmem.wdata(31, 24)
+  }
 }
