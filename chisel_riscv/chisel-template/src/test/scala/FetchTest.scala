6a35d678283252852c1fba94e0e25b6cba141276
msBRF65 <msaito6525@gmail.com>
2022-11-26 21:15:26 +0900

    chore: change package name

--- chisel_riscv/chisel-template/src/test/scala/FetchTest.scala
+++ chisel_riscv/chisel-template/src/test/scala/FetchTest.scala
@@ -1,11 +1,10 @@
-package fetch
+package cpu
 
 import chisel3._
-
 import org.scalatest._
 import chiseltest._
 
-class HexTest extends FlatSpec with ChiselScalatestTester {
+class HexFetchTest extends FlatSpec with ChiselScalatestTester {
   "mycpu" should "work through hex" in {
     test(new Top) { c =>
       while (!c.io.exit.peek().litToBoolean) {
