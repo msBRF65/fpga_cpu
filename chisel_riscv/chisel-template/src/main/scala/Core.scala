28065cba0997048e8b4c40c8847569f6d389d481
msBRF65 <msaito6525@gmail.com>
2022-11-27 22:26:49 +0900

    add: csr inst

--- chisel_riscv/chisel-template/src/main/scala/Core.scala
+++ chisel_riscv/chisel-template/src/main/scala/Core.scala
@@ -48,22 +48,24 @@ class Core extends Module {
   val imm_j_sext = Cat(Fill(11, imm_j(19)), imm_j, 0.U(1.U))
   val imm_u = inst(31, 12)
   val imm_u_shifted = Cat(imm_u, Fill(12, 0.U))
+  val imm_z = inst(19, 15)
+  val imm_z_uext = Cat(Fill(27, 0.U), imm_z)
 
   val csignals = ListLookup(
     inst,
-    List(ALU_X, OP1_RS1, OP2_RS2, MEM_X, REN_X, WB_X),
+    List(ALU_X, OP1_RS1, OP2_RS2, MEM_X, REN_X, WB_X, CSR_X),
     Array(
-      LW -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_MEM),
-      SW -> List(ALU_ADD, OP1_RS1, OP2_IMS, MEM_S, REN_X, WB_X),
-      ADD -> List(ALU_ADD, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU),
-      ADDI -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_ALU),
-      SUB -> List(ALU_SUB, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU),
-      AND -> List(ALU_AND, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU),
-      OR -> List(ALU_OR, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU),
-      XOR -> List(ALU_XOR, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU),
-      ANDI -> List(ALU_AND, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_ALU),
-      ORI -> List(ALU_OR, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_ALU),
-      XORI -> List(ALU_XOR, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_ALU),
+      LW -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_MEM, CSR_X),
+      SW -> List(ALU_ADD, OP1_RS1, OP2_IMS, MEM_S, REN_X, WB_X, CSR_X),
+      ADD -> List(ALU_ADD, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU, CSR_X),
+      ADDI -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_ALU, CSR_X),
+      SUB -> List(ALU_SUB, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU, CSR_X),
+      AND -> List(ALU_AND, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU, CSR_X),
+      OR -> List(ALU_OR, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU, CSR_X),
+      XOR -> List(ALU_XOR, OP1_RS1, OP2_RS2, MEM_X, REN_S, WB_ALU, CSR_X),
+      ANDI -> List(ALU_AND, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_ALU, CSR_X),
+      ORI -> List(ALU_OR, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_ALU, CSR_X),
+      XORI -> List(ALU_XOR, OP1_RS1, OP2_IMI, MEM_X, REN_S, WB_ALU, CSR_X),
       SLL -> List(ALU_SLL, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU, CSR_X),
       SRL -> List(ALU_SRL, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU, CSR_X),
       SRA -> List(ALU_SRA, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU, CSR_X),
@@ -83,10 +85,16 @@ class Core extends Module {
       JAL -> List(ALU_ADD, OP1_PC, OP2_IMJ, MEN_X, REN_S, WB_PC, CSR_X),
       JALR -> List(ALU_JALR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_PC, CSR_X),
       LUI -> List(ALU_ADD, OP1_X, OP2_IMU, MEN_X, REN_S, WB_ALU, CSR_X),
-      AUIPC -> List(ALU_ADD, OP1_PC, OP2_IMU, MEN_X, REN_S, WB_ALU, CSR_X)
+      AUIPC -> List(ALU_ADD, OP1_PC, OP2_IMU, MEN_X, REN_S, WB_ALU, CSR_X),
+      CSRRW -> List(ALU_COPY1, OP1_RS1, OP2_X, MEN_X, REN_S, WB_CSR, CSR_W),
+      CSRRWI -> List(ALU_COPY1, OP1_IMZ, OP2_X, MEN_X, REN_S, WB_CSR, CSR_W),
+      CSRRS -> List(ALU_COPY1, OP1_RS1, OP2_X, MEN_X, REN_S, WB_CSR, CSR_S),
+      CSRRSI -> List(ALU_COPY1, OP1_IMZ, OP2_X, MEN_X, REN_S, WB_CSR, CSR_S),
+      CSRRC -> List(ALU_COPY1, OP1_RS1, OP2_X, MEN_X, REN_S, WB_CSR, CSR_C),
+      CSRRCI -> List(ALU_COPY1, OP1_IMZ, OP2_X, MEN_X, REN_S, WB_CSR, CSR_C)
     )
   )
-  val exe_fun :: op1_sel :: op2_sel :: mem_wen :: rfwen :: wb_sel :: Nil =
+  val exe_fun :: op1_sel :: op2_sel :: mem_wen :: rfwen :: wb_sel :: csr_cmd :: Nil =
     csignals
   val op1_data =
     MuxCase(
@@ -123,7 +131,8 @@ class Core extends Module {
       (exe_fun === ALU_SLA) -> (op1_data.asSInt() >> op2_data(4, 0)).asUInt(),
       (exe_fun === ALU_SLT) -> (op1_data.asSInt() < op2_data.asSInt()).asUInt(),
       (exe_fun === ALU_SLTU) -> (op1_data < op2_data).asUInt(),
-      (exe_fun === ALU_JALR) -> ((op1_data + op2_data) & ~1.U(WORD_LEN.W))
+      (exe_fun === ALU_JALR) -> ((op1_data + op2_data) & ~1.U(WORD_LEN.W)),
+      (exe_fun === ALU_COPY1) -> op1_data
     )
   )
   // branch
@@ -145,12 +154,32 @@ class Core extends Module {
   io.dmem.wen := mem_wen
   io.dmem.wdata := rs2_data
 
+  // CSR
+  val csr_regfile = Mem(4096, UInt(WORD_LEN.W))
+  val csr_addr = inst(31, 20)
+
+  val csr_rdata = csr_regfile(csr_addr)
+
+  val csr_wdata = MuxCase(
+    0.U(WORD_LEN.W),
+    Seq(
+      (csr_cmd === CSR_W) -> op1_data,
+      (csr_cmd === CSR_S) -> (csr_rdata | op1_data),
+      (csr_cmd === CSR_C) -> (csr_rdata & ~op1_data)
+    )
+  )
+
+  when(csr_cmd > 0.U) {
+    csr_regfile(csr_addr) := csr_wdata
+  }
+
   // WB
   val wb_data = MuxCase(
     alu_out,
     Seq(
       (wb_sel === WB_MEM) -> io.dmem.rdata,
-      (wb_sel === WB_PC) -> pc_plus4
+      (wb_sel === WB_PC) -> pc_plus4,
+      (wb_sel === WB_CSR) -> csr_rdata
     )
   )
 
